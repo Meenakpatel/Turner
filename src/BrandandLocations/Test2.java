35a4eeea8646b85399800b29fe468798be4aa1ab
Jay <meena.pinkesh@gmai.com>
2016-07-08 16:09:25 -0400

    TestCase updated

---
+++ src/BrandandLocations/Test2.java
@@ -0,0 +1,28 @@
+package BrandandLocations;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+
+public class Test2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Jay\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		TakesScreenshot ts =(TakesScreenshot)driver;
+		File src=ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("my.jpg"));
+	}
+	
+	
+
+}
